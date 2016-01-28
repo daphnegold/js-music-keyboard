abc07301f2f9ae8879653b526ebd164c0ddd8db4
Daphne <dg1181@nyu.edu>
2016-01-27 18:16:58 -0800

    Keyboard done.

--- noise.js
+++ noise.js
@@ -1,3 +1,26 @@
 $(document).ready( function() {
-  // your code here
+  var notes = { "c": 67, "d": 68, "e": 69, "f": 70, "g": 71, "a": 65, "b": 66 };
+
+  Object.keys(notes).forEach(function(note) {
+    $("." + note).click(function () {
+        var audio = $("#" + note + "Audio")[0];
+        audio.load();
+        audio.play();
+    });
+
+    $(document).keydown(function(e) {
+      if (e.keyCode == notes[note]) {
+        var audio = $("#" + note + "Audio")[0];
+        audio.load();
+        audio.play();
+        $("." + note).addClass("hover");
+      }
+    });
+
+    $(document).keyup(function(e) {
+      if (e.keyCode == notes[note]) {
+        $("." + note).removeClass("hover");
+      }
+    });
+  });
 });
